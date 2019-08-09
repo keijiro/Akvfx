78050b17359d07b5b6eb0c98244fd08c813dec5c
Keijiro Takahashi <keijiro@gmail.com>
2019-08-10 01:26:03 +0900

    Point cloud support

---
+++ Assets/Converter.shader
@@ -0,0 +1,55 @@
+﻿Shader "Hidden/Converter"
+{
+    CGINCLUDE
+
+    #include "UnityCG.cginc"
+
+    texture2D _MainTex;
+    float2 _Dimensions;
+
+    void Vertex(
+        float4 position : POSITION,
+        out float4 positionOut : SV_Position,
+        inout float2 texCoord : TEXCOORD0
+    )
+    {
+        positionOut = UnityObjectToClipPos(position);
+    }
+
+    float4 Fragment(
+        float4 position : SV_Position,
+        float2 texCoord : TEXCOORD0
+    ) : SV_Target
+    {
+        uint sx = texCoord.x * _Dimensions.x;
+        uint sy = texCoord.y * _Dimensions.y;
+
+        sx *= 6;
+
+        int b0 = _MainTex[uint2(sx + 0, sy)] * 255;
+        int b1 = _MainTex[uint2(sx + 1, sy)] * 255;
+        int b2 = _MainTex[uint2(sx + 2, sy)] * 255;
+        int b3 = _MainTex[uint2(sx + 3, sy)] * 255;
+        int b4 = _MainTex[uint2(sx + 4, sy)] * 255;
+        int b5 = _MainTex[uint2(sx + 5, sy)] * 255;
+
+        float3 xyz = float3(b0 + (b1 << 8), b2 + (b3 << 8), b4 + (b5 << 8));
+        xyz = (xyz < 0x8000 ? xyz : xyz - 0x10000) / 0x8000;
+
+        return float4(xyz, 1);
+    }
+
+    ENDCG
+
+    SubShader
+    {
+        Cull Off ZWrite Off ZTest Always
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex Vertex
+            #pragma fragment Fragment
+            ENDCG
+        }
+    }
+}
--- Assets/Tester.cs
+++ Assets/Tester.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UI = UnityEngine.UI;
 using GraphicsFormat = UnityEngine.Experimental.Rendering.GraphicsFormat;
 using IntPtr = System.IntPtr;
 using TimeSpan = System.TimeSpan;
@@ -9,21 +8,67 @@ namespace K4aTest
 {
     sealed class Tester : MonoBehaviour
     {
-        [SerializeField] UI.RawImage _colorUI = null;
-        [SerializeField] UI.RawImage _depthUI = null;
+        #region Editable attributes
+
+        [SerializeField] RenderTexture _colorTexture = null;
+        [SerializeField] RenderTexture _positionTexture = null;
+        [SerializeField, HideInInspector] Shader _shader = null;
+
+        #endregion
+
+        #region K4a objects
 
         Device _device;
+        Transformation _transformation;
 
-        Texture2D _colorTexture;
-        Texture2D _depthTexture;
+        #endregion
+
+        #region UnityEngine objects
+
+        Material _material;
+        Texture2D _colorTemp;
+        Texture2D _pointCloud;
+
+        #endregion
+
+        #region Private functions
+
+        unsafe void UpdateExternalTextures(Image color, Image pointCloud)
+        {
+            var cmem = color.Memory;
+
+            using (var handle = cmem.Pin())
+                _colorTemp.LoadRawTextureData((IntPtr)handle.Pointer, cmem.Length);
+
+            var pmem = pointCloud.Memory;
+
+            using (var handle = pmem.Pin())
+                _pointCloud.LoadRawTextureData((IntPtr)handle.Pointer, pmem.Length);
+
+            _colorTemp.Apply();
+            _pointCloud.Apply();
+
+            _material.SetTexture("_MainTex", _pointCloud);
+            _material.SetVector("_Dimensions", new Vector2(color.WidthPixels, color.HeightPixels));
+
+            Graphics.Blit(_colorTemp, _colorTexture);
+            Graphics.Blit(_pointCloud, _positionTexture, _material, 0);
+        }
+
+        #endregion
+
+        #region MonoBehaviour implementation
 
         void Start()
         {
+            // If there is no available device, do nothing.
             if (Device.GetInstalledCount() == 0) return;
 
+            // Open the default device.
             _device = Device.Open();
             if (_device == null) return;
 
+            // Start capturing with our settings.
             _device.StartCameras(
                 new DeviceConfiguration {
                     ColorFormat = ImageFormat.ColorBGRA32,
@@ -33,44 +78,54 @@ namespace K4aTest
                 }
             );
 
-            _colorTexture = new Texture2D(2048, 1536, GraphicsFormat.B8G8R8A8_SRGB, 0, 0);
-            _depthTexture = new Texture2D(640, 576, GraphicsFormat.R16_UNorm, 0, 0);
+            // Prepare the transformation object.
+            _transformation = new Transformation(_device.GetCalibration());
 
-            _colorUI.texture = _colorTexture;
-            _depthUI.texture = _depthTexture;
+            // Temporary objects for convertion shader
+            _material = new Material(_shader);
+            _colorTemp = new Texture2D(2048, 1536, GraphicsFormat.B8G8R8A8_SRGB, 0);
+            _pointCloud = new Texture2D(2048 * 6, 1536, GraphicsFormat.R8_UNorm, 0);
         }
 
         void OnDestroy()
         {
-            if (_colorTexture != null) Destroy(_colorTexture);
-            if (_depthTexture != null) Destroy(_depthTexture);
+            if (_material != null) Destroy(_material);
+            if (_colorTemp != null) Destroy(_colorTemp);
+            if (_pointCloud != null) Destroy(_pointCloud);
 
+            _transformation?.Dispose();
             _device?.StopCameras();
             _device?.Dispose();
         }
 
-        unsafe void Update()
+        void Update()
         {
+            // Check if the device is ready.
+            if (_device == null || _transformation == null) return;
+
             try
             {
+                // Capture a frame with 1/15 sec timeout.
+                // TODO: Threading
                 using (var capture = _device.GetCapture(TimeSpan.FromSeconds(1.0 / 15)))
                 {
-                    var color = capture.Color.Memory;
-                    using (var handle = color.Pin())
-                        _colorTexture.LoadRawTextureData((IntPtr)handle.Pointer, color.Length);
-
-                    var depth = capture.Depth.Memory;
-                    using (var handle = depth.Pin())
-                        _depthTexture.LoadRawTextureData((IntPtr)handle.Pointer, depth.Length);
+                    // Transform the depth image to the color perspective.
+                    using (var depth = _transformation.DepthImageToColorCamera(capture))
+                    {
+                        // Unproject the depth samples and reconstruct a point cloud.
+                        using (var pointCloud = _transformation.DepthImageToPointCloud(depth, CalibrationDeviceType.Color))
+                        {
+                            UpdateExternalTextures(capture.Color, pointCloud);
+                        }
+                    }
                 }
             }
             catch (System.TimeoutException)
             {
                 // Ignore timeouts
             }
-
-            _colorTexture.Apply();
-            _depthTexture.Apply();
         }
+
+        #endregion
     }
 }
