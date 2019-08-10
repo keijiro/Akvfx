d14b045c5594275e13bde26abe59a4cc742c495a
Keijiro Takahashi <keijiro@gmail.com>
2019-08-10 15:54:01 +0900

    threading

--- Assets/Akvfx/PointCloudBaker.cs
+++ Assets/Akvfx/PointCloudBaker.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using GraphicsFormat = UnityEngine.Experimental.Rendering.GraphicsFormat;
 using IntPtr = System.IntPtr;
 using TimeSpan = System.TimeSpan;
+using System.Threading;
 using Microsoft.Azure.Kinect.Sensor;
 
 namespace Akvfx
@@ -31,16 +32,57 @@ namespace Akvfx
 
         #endregion
 
+        #region Threaded capture
+
+        Thread _captureThread;
+        bool _terminate;
+        AutoResetEvent _nextCapture;
+        (Image color, Image pointCloud) _captured;
+
+        void CaptureThread()
+        {
+            while (!_terminate)
+            {
+                try
+                {
+                    // Try to capture a frame with 1/15 sec timeout.
+                    using (var capture = _device.GetCapture(TimeSpan.FromSeconds(1.0 / 15)))
+                    {
+                        // Transform the depth image to the color perspective.
+                        using (var depth = _transformation.DepthImageToColorCamera(capture))
+                        {
+                            // Unproject the depth samples and reconstruct a point cloud.
+                            using (var pointCloud = _transformation.DepthImageToPointCloud
+                                (depth, CalibrationDeviceType.Color))
+                            {
+                                // Send the results to the main thread.
+                                _captured = (capture.Color, pointCloud);
+
+                                // Wait the main thead consumes them.
+                                _nextCapture.WaitOne();
+                            }
+                        }
+                    }
+                }
+                catch (System.TimeoutException)
+                {
+                    Thread.Sleep(100);
+                }
+            }
+        }
+
+        #endregion
+
         #region Private functions
 
-        unsafe void UpdateExternalTextures(Image color, Image pointCloud)
+        unsafe void UpdateTexturesByCapturedData()
         {
-            var cmem = color.Memory;
+            var cmem = _captured.color.Memory;
 
             using (var handle = cmem.Pin())
                 _colorTemp.LoadRawTextureData((IntPtr)handle.Pointer, cmem.Length);
 
-            var pmem = pointCloud.Memory;
+            var pmem = _captured.pointCloud.Memory;
 
             using (var handle = pmem.Pin())
                 _pointCloud.LoadRawTextureData((IntPtr)handle.Pointer, pmem.Length);
@@ -49,7 +91,10 @@ namespace Akvfx
             _pointCloud.Apply();
 
             _material.SetTexture("_SourceTexture", _pointCloud);
-            _material.SetVector("_Dimensions", new Vector2(color.WidthPixels, color.HeightPixels));
+            _material.SetVector("_Dimensions", new Vector2(
+                _captured.color.WidthPixels,
+                _captured.color.HeightPixels
+            ));
 
             Graphics.Blit(_colorTemp, _colorTexture);
             Graphics.Blit(null, _positionTexture, _material, 0);
@@ -85,10 +130,22 @@ namespace Akvfx
             _material = new Material(_shader);
             _colorTemp = new Texture2D(2048, 1536, GraphicsFormat.B8G8R8A8_SRGB, 0);
             _pointCloud = new Texture2D(2048 * 6, 1536, GraphicsFormat.R8_UNorm, 0);
+
+            // Start the capture thread.
+            _captureThread = new Thread(CaptureThread);
+            _nextCapture = new AutoResetEvent(false);
+            _captureThread.Start();
         }
 
         void OnDestroy()
         {
+            if (_captureThread != null)
+            {
+                _terminate = true;
+                _nextCapture.Set();
+                _captureThread.Join();
+            }
+
             if (_material != null) Destroy(_material);
             if (_colorTemp != null) Destroy(_colorTemp);
             if (_pointCloud != null) Destroy(_pointCloud);
@@ -100,29 +157,11 @@ namespace Akvfx
 
         void Update()
         {
-            // Check if the device is ready.
-            if (_device == null || _transformation == null) return;
-
-            try
+            if (_captured.color != null && _captured.pointCloud != null)
             {
-                // Capture a frame with 1/15 sec timeout.
-                // TODO: Threading
-                using (var capture = _device.GetCapture(TimeSpan.FromSeconds(1.0 / 15)))
-                {
-                    // Transform the depth image to the color perspective.
-                    using (var depth = _transformation.DepthImageToColorCamera(capture))
-                    {
-                        // Unproject the depth samples and reconstruct a point cloud.
-                        using (var pointCloud = _transformation.DepthImageToPointCloud(depth, CalibrationDeviceType.Color))
-                        {
-                            UpdateExternalTextures(capture.Color, pointCloud);
-                        }
-                    }
-                }
-            }
-            catch (System.TimeoutException)
-            {
-                // Ignore timeouts
+                UpdateTexturesByCapturedData();
+                _captured = (null, null);
+                _nextCapture.Set();
             }
         }
 
