af0aef080873f5a226f41b76fd213533262138d2
Keijiro Takahashi <keijiro@gmail.com>
2020-02-04 17:30:21 +0900

    Increase delta to get smoother normals

--- Assets/Akvfx/SurfaceHelper.hlsl
+++ Assets/Akvfx/SurfaceHelper.hlsl
@@ -13,10 +13,10 @@ void K4aVertex_float(
     int ty = uv.y * th;
 
     float4 p = positionMap.Load(int3(tx, ty, 0));
-    float3 p_x0 = positionMap.Load(int3(tx - 1, ty, 0)).xyz;
-    float3 p_x1 = positionMap.Load(int3(tx + 1, ty, 0)).xyz;
-    float3 p_y0 = positionMap.Load(int3(tx, ty - 1, 0)).xyz;
-    float3 p_y1 = positionMap.Load(int3(tx, ty + 1, 0)).xyz;
+    float3 p_x0 = positionMap.Load(int3(tx - 4, ty, 0)).xyz;
+    float3 p_x1 = positionMap.Load(int3(tx + 4, ty, 0)).xyz;
+    float3 p_y0 = positionMap.Load(int3(tx, ty - 4, 0)).xyz;
+    float3 p_y1 = positionMap.Load(int3(tx, ty + 4, 0)).xyz;
 
     outPosition = p.xyz;
     outNormal = -normalize(cross(p_x1 - p_x0, p_y1 - p_y0));
